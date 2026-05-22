d4f61afb3c31b874c358cfa479fbb9cce3a8b253
Kshitij Jha <connect.kshitijjha@gmail.com>
2026-05-23 01:54:14 +0530

    change 3

--- index.js
+++ index.js
@@ -119,12 +119,6 @@ app.get("/Posts", (req,res) =>{
   res.render("Posts", {posts: posts, topics: topics, user: user.length > 0 ? user[user.length - 1] : null});
 });
 
-app.get("/Posts/:username/MyPosts", (req, res) =>{
-  let {username} = req.params;
-  let userPosts = posts.filter(p => p.user === username);
-  res.render("MyPosts", {posts: userPosts, user: user.length > 0 ? user[user.length - 1] : null});
-});
-
 app.patch("/Posts/:id", (req, res) =>{
   let {id} = req.params;
   let newContent = req.body.content;
@@ -147,6 +141,10 @@ app.delete("/Posts/:id", (req, res) => {
   res.redirect("/Posts");
 });
 
-
+app.get("/Posts/:username/MyPosts", (req, res) =>{
+  let {username} = req.params;
+  let userPosts = posts.filter(p => p.user === username);
+  res.render("MyPosts", {posts: userPosts, user: user.length > 0 ? user[user.length - 1] : null});
+});
 
 
